face5d9e964470971bc1fa37ad2257b864348b85
Andre Koga <social@koga.one>
2022-09-14 13:41:27 -0400

    second banner ideas

--- components/Panel1.tsx
+++ components/Panel1.tsx
@@ -2,7 +2,7 @@ const Panel1 = () => {
   return (
     <div className="panel-1">
       <p className="n1">- developer, designer, leader -</p>
-      <p className="n2">Koga</p>
+      <p className="n2">KOGA</p>
       <p className="n3">- nice to meet you! -</p>
     </div>
   );
--- components/Panel2.tsx
+++ components/Panel2.tsx
@@ -1,5 +1,18 @@
 const Panel2 = () => {
-  return <div id="about" className="panel-2"></div>;
+  return (
+    <div id="about" className="panel-2">
+      <div className="n1">
+        <h2>Born in Brazil, shaped by the Japanese culture.</h2>
+        <p>André Hiroshi Koga</p>
+      </div>
+      <div className="n2">
+        <div></div>
+      </div>
+      <div className="n3">
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Panel2;
