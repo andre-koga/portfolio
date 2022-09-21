21ebdba1124d7cdb40da9765e573c3e244fdde57
Andre Koga <social@koga.one>
2022-09-20 23:37:30 -0400

    lil adjustments

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,30 +2,33 @@ const Introduction = () => {
   return (
     <section
       id="introduction"
-      className="flex min-h-[90vh] flex-col items-center justify-center bg-stone-300 px-[4vmin] py-[8vmin]"
+      className="flex min-h-[90vh] flex-col items-center justify-center bg-stone-300 px-[4vmin] pt-[6vh] pb-[6vh]"
     >
-      <p className="text-center font-rox text-[12vmin] lowercase">
+      {/* <h1 className="active text-scroll absolute mb-[12vmin] whitespace-nowrap px-8 text-center font-flex text-[16vmin] lowercase leading-[15vmin]">
+        a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k
+      </h1>
+      <h1 className="active text-late-scroll absolute mb-[12vmin] whitespace-nowrap px-8 text-center font-flex text-[16vmin] lowercase leading-[15vmin]">
+        a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k a⋆k
+      </h1> */}
+      {/* <h1 className="font-flex text-[30vmin] leading-[40vmin] text-pepper">
+        ⋆
+      </h1> */}
+      <p className="text-center font-rox text-[11vmin] lowercase">
         <img
-          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[12vmin]"
+          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[16vmin] rotate-6 rounded-xl"
           src="/images/me.jpg"
           alt=""
         />
-        <span>Web-designer ― full-stack developer ― digital artist.</span>
+        <span>Web-designer, full-stack developer, digital artist</span>
         <img
-          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[12vmin]"
+          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[16vmin] -rotate-2 rounded-xl"
           src="/images/light.png"
           alt=""
         />
-        <span>Strives to do his best in any task.</span>
+        <span>Strives to do his best in any task ― Based in Atlanta</span>
         <img
-          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[12vmin]"
+          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[16vmin] rotate-3 rounded-xl"
           src="/images/atlanta.jpg"
-          alt=""
-        />
-        Based in Atlanta ― São Paulo.
-        <img
-          className="mx-[4vmin] mb-[2vmin] -mt-[2vmin] inline-block h-[12vmin]"
-          src="/images/paulista.jpg"
           alt=""
         />
       </p>
